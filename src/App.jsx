5a44d0abf8b0c29c67e4bf33ddd8df005a4be3ec
Azrul Saleh <azrulsaleh@me.com>
2026-04-03 01:11:39 +0800

    added base css files + responsive card bg. updated favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 function App() {
-  return (
-    <div className="flex h-screen items-center justify-center bg-slate-200">
-      <h1 className="text-6xl tracking-widest">
-        <span className="font-bold text-sky-800">DREAM</span>
-        <span className="font-extralight text-cyan-700">KEEPER</span>
-      </h1>
-    </div>
-  )
+	return (
+		<section className="h-screen flex justify-center items-center">
+			<div className="_bg-card">
+				<h1 className="text-center">
+					<span className="text-sky-800">DREAM</span>
+					<span className="font-extralight text-cyan-700">KEEPER</span>
+				</h1>
+			</div>
+		</section>
+	)
 }
 
 export default App
