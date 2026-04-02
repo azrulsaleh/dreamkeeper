cb55f781f51caec7be0b0f0f64ed163996da162a
Azrul Saleh <azrulsaleh@me.com>
2026-04-02 17:19:37 +0800

    installed react, tailwind, tone.js + wavesurfer.js

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-slate-200">
+      <h1 className="text-6xl tracking-widest">
+        <span className="font-bold text-sky-800">DREAM</span>
+        <span className="font-extralight text-cyan-700">KEEPER</span>
+      </h1>
+    </div>
+  )
+}
+
+export default App
